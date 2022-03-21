5b56f44f54493e266fa930b7aecf2d31a778804c
Charlie <charlie.oconnor98@gmail.com>
2022-03-21 16:14:16 +0000

    Added class/function structure to 'http_get_requests.py'

--- https_get_requests.py
+++ https_get_requests.py
@@ -1,18 +1,22 @@
 import requests
 import json
 
-json_data = []
+class get_requests:
+    def create_url_list():
+        url_list = [
+                    requests.get("http://www.bbc.co.uk/iplayer"),
+                    requests.get("http://checkip.amazonaws.com"),
+                    requests.get("https://www.bbc.co.uk/missing/thing"),
+                    requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juve nile_Ragdoll.jpg/220px-Juvenile_Ragdoll.jpg"),
+                    requests.get("https://site.mockito.org/")
+                    ]
+        return url_list
 
-url_list = [
-requests.get("http://www.bbc.co.uk/iplayer"),
-requests.get("http://checkip.amazonaws.com"),
-requests.get("https://www.bbc.co.uk/missing/thing"),
-requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juve nile_Ragdoll.jpg/220px-Juvenile_Ragdoll.jpg"),
-requests.get("https://site.mockito.org/")
-]
+    def create_json_data():           
+        json_data = []
+        temp_url_list = get_requests.create_url_list()
+        for url in temp_url_list:
+            json_data.append({"Url": url.url, "Status-code": url.status_code, "Content-Length": len(url.content)})
+        return json_data
 
-for url in url_list:
-    json_data.append({"Url": url.url, "Status-code": url.status_code, "Content-Length": len(url.content)})
-
-
-print(json.dumps(json_data, indent=2))
+print(json.dumps(get_requests.create_json_data(), indent=2))
