16fdee6a024f9766a290bd462582c59c731a0f70
Charlie <charlie.oconnor98@gmail.com>
2022-03-21 15:58:51 +0000

    Update https_get_requests.py

--- https_get_requests.py
+++ https_get_requests.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+json_data = []
+
 url_list = [
 requests.get("http://www.bbc.co.uk/iplayer"),
 requests.get("http://checkip.amazonaws.com"),
@@ -8,8 +10,6 @@ requests.get("https://www.bbc.co.uk/missing/thing"),
 requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juve nile_Ragdoll.jpg/220px-Juvenile_Ragdoll.jpg"),
 requests.get("https://site.mockito.org/")
 ]
-
-json_data = []
 
 for url in url_list:
     json_data.append({"Url": url.url, "Status-code": url.status_code, "Content-Length": len(url.content)})
